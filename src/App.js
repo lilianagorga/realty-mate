89e944fcc25ab4418ff18644694f637dda411479
Liliana Gorga <gorgaliliana87@gmail.com>
2024-02-15 10:14:24 +0100

    Implement Google Maps Street View API to enhance the mapping features of the application. Utilized the @react-google-maps/api library with the useJsApiLoader hook for efficient async loading

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Map from './Map';
 import PlacesAutocomplete from './PlacesAutocomplete';
-import { LoadScript } from '@react-google-maps/api';
+import { useJsApiLoader } from '@react-google-maps/api';
 
 const libraries = ['places'];
 
@@ -9,21 +9,52 @@ function App() {
   const [mapCenter, setMapCenter] = useState({ lat: 41.8719, lng: 12.5674 });
   const [zoom, setZoom] = useState(6);
 
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAP_API_KEY,
+    libraries: libraries
+  });
+
+  if (!isLoaded) {
+    return <div>Loading...</div>;
+  }
+  
   const handlePlaceSelect = (place) => {
     if (place.geometry) {
-      setMapCenter({
+      const location = {
         lat: place.geometry.location.lat(),
         lng: place.geometry.location.lng(),
-      });
+      };
+
+      setMapCenter(location);
       setZoom(15);
+      checkStreetViewAvailability(location);
     }
   };
 
+  const onStreetViewPositionChanged = (newPosition) => {
+    if (Math.abs(newPosition.lat - mapCenter.lat) > 0.0001 || Math.abs(newPosition.lng - mapCenter.lng) > 0.0001) {
+      console.log('New Position Street View:', newPosition);
+      setMapCenter(newPosition);
+    }
+  };
+  
+  const checkStreetViewAvailability = (location) => {
+    const sv = new window.google.maps.StreetViewService();
+
+    sv.getPanorama({ location, radius: 50 }, (data, status) => {
+      if (status === window.google.maps.StreetViewStatus.OK) {
+        console.log('Street View available.', data);
+      } else {
+        console.error('Street View not available for this position.', status);
+      }
+    });
+  };
+
   return (
-    <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAP_API_KEY} libraries={libraries}>
-      <PlacesAutocomplete onPlaceSelected={handlePlaceSelect} />
-      <Map center={mapCenter} zoom={zoom} />
-  </LoadScript>
+    <>
+    <PlacesAutocomplete onPlaceSelected={handlePlaceSelect} />
+    <Map center={mapCenter} zoom={zoom} onPositionChange={onStreetViewPositionChanged}/>
+  </>
   );
 }
 
--- src/Map.js
+++ src/Map.js
@@ -1,11 +1,38 @@
-import React from 'react';
-import { GoogleMap, Marker } from '@react-google-maps/api';
+import React, { useRef, useState, useEffect } from 'react';
+import { GoogleMap, Marker, StreetViewPanorama } from '@react-google-maps/api';
 
-function Map({ center, zoom }) {
+function Map({ center, zoom, onPositionChange }) {
   const containerStyle = {
     width: '100%',
     height: '70vh'
   };
+
+  const panoramaRef = useRef(null);
+  const [panoramaPosition, setPanoramaPosition] = useState(center);
+  
+  const panoramaOptions = {
+    position: center,
+    pov: { heading: 100, pitch: 0 },
+    motionTracking: false, 
+    enableCloseButton: true,
+    visible: true,
+  };
+  
+  const handlePositionChange = () => {
+    if (window.google && window.google.maps && panoramaRef.current) {
+      const newPosition = panoramaRef.current.getPosition().toJSON();
+      if (newPosition.lat !== panoramaPosition.lat || newPosition.lng !== panoramaPosition.lng) {
+        setPanoramaPosition(newPosition);
+        onPositionChange(newPosition);
+      }
+    }
+  }
+
+  useEffect(() => {
+    if (panoramaRef.current && (center.lat !== panoramaPosition.lat || center.lng !== panoramaPosition.lng)) {
+      panoramaRef.current.setPosition(center);
+    }
+  }, [center, panoramaPosition]);
 
   return (
     <GoogleMap
@@ -14,6 +41,7 @@ function Map({ center, zoom }) {
       zoom={zoom}
   >
     <Marker position={center} />
+    <StreetViewPanorama options={panoramaOptions} onPositionChanged={handlePositionChange} />
   </GoogleMap>
   );
 }
