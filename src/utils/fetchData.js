2b1b5991b7a879fb48ef9d45d26cb0cb35546726
Liliana Gorga <gorgaliliana87@gmail.com>
2024-06-14 13:17:38 +0200

    minor refactor team fetchData

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,6 +1,11 @@
 import axios from "axios";
 
 export const getTeams = async () => {
-  const response = await axios.get(`${import.meta.env.VITE_API_URL}/teams`);
+  const token = localStorage.getItem('token');
+    const response = await axios.get(`${import.meta.env.VITE_API_URL}/teams`, {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    });
   return response.data || [];
 };
